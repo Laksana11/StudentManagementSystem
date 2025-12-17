b12953eb1c7acaedb8da785219acb7a00e2c82cc
lakshana <laksana@airentosoft.com>
2025-12-17 20:44:28 +0530

    changes

--- frontend/src/components/StudentList.jsx
+++ frontend/src/components/StudentList.jsx
@@ -79,7 +79,7 @@ function StudentList({ students, loading, searchTerm, onSearchChange, onEdit, on
                     </Typography>
                     {!searchTerm && (
                         <Button variant="contained" startIcon={<AddIcon />} onClick={onAddNew}>
-                            Add Your First Student
+                            Add Your First Student Here
                         </Button>
                     )}
                 </Paper>
